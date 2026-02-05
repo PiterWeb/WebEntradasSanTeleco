5ff2772a8d96590de49d991be5e8ecd48598eba4
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-02-05 20:23:03 +0100

    Use only vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,11 @@ import 'dotenv/config';
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import node from "@astrojs/node";
 import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
 
-  adapter: process.env.VERCEL === "TRUE" ? vercel() : node({
-    mode: "standalone"
-  }),
+  adapter: vercel()
 });
