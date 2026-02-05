22e11e2ecb1c96d6c0dea1666e73ae9f81b8c14e
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-02-05 20:40:46 +0100

    Update db connection to use url and authtoken

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -2,7 +2,12 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 import { reservasTable } from './schema';
 
-const db = drizzle(process.env.DB_FILE_NAME!);
+const db = drizzle({
+  connection: {
+    url: process.env.DB_FILE_NAME!,
+    authToken: process.env.DB_TOKEN
+  }
+});
 
 export async function GetReservas() {
   return await db.select().from(reservasTable)
