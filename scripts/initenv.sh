c3286861ac8fd957d0970b4730a3c24ec49b3085
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-03-18 16:44:45 +0100

    Create initenv.sh

---
+++ scripts/initenv.sh
@@ -0,0 +1,11 @@
+
+HMAC_KEY=$(openssl rand -base64 48)
+
+FILE="LOCAL=true
+DB_FILE_NAME=file:reservas.db
+DB_TOKEN=xxx
+HMAC_KEY=$HMAC_KEY
+EMAIL_ADDRESS=santeleco@daat.uvigo.es
+RESEND_API_KEY=re_xxx"
+
+echo "$FILE" > .env.example
