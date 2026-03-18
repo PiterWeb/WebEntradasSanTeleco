f94ac202f13c8834b5d46214a14f79dbe6cf3eb9
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-03-18 20:17:59 +0100

    Use of LOCAL env var to switch between vercel/node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,24 @@
 // @ts-check
+import 'dotenv/config';
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-import vercel from "@astrojs/vercel";
+const adapter = async function() {
+  if (process.env.LOCAL === "true") {
+    const nodejs = (await import("@astrojs/node")).default
+    
+    return nodejs({
+      mode: "standalone"
+    })
+  }
+  
+  const vercel = (await import("@astrojs/vercel")).default
+  
+  return vercel()
+}()
 
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
-
-  adapter: vercel()
+  adapter: await adapter
 });
