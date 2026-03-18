da533f42236ca44304d216ce9ca840ab74d23930
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-03-18 16:33:34 +0100

    Soporte de cambio de dialecto entre modo LOCAL y "otro"

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   out: './drizzle',
   schema: './src/lib/db/schema.ts',
-  dialect: 'turso', // cambiar a sqlite para usar en local
+  dialect: process.env.LOCAL === "true" ? 'sqlite' : 'turso', // cambiar a sqlite para usar en local
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
-    authToken: process.env.DB_TOKEN!, // eliminar para usar en local o usar token
+    [process.env.LOCAL === "true" ? "token" : "authToken"]: process.env.DB_TOKEN!, // eliminar para usar en local o usar token
   },
 });
